cd96afc142e5162bb3774331668bed147b72559d
shalabyelectronics <mhdshalabystar@gmail.com>
2021-09-30 22:13:15 +0300

    create the snake controller

--- main.py
+++ main.py
@@ -9,9 +9,37 @@ screen.bgcolor("black")
 
 screen.listen()
 
+
+# TODO 2 Create the Snake controller
+
 def move_forward():
     sha.forward(10)
 
-screen.onkey(key="w", fun=move_forward)
+
+def turn_left():
+    sha.setheading(180)
+
+
+def turn_right():
+    sha.setheading(0)
+
+
+def turn_down():
+    sha.setheading(270)
+
+
+def turn_up():
+    sha.setheading(90)
+
+
+screen.onkey(key="w", fun=turn_up)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="s", fun=turn_down)
+
+while True:
+    sha.speed(10)
+    sha.penup()
+    move_forward()
 
 screen.exitonclick()
