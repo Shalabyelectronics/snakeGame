daf1c09438903bd157d04c26fbc670d04cee5deb
shalabyelectronics <mhdshalabystar@gmail.com>
2021-09-30 23:06:34 +0300

    create the snake body by using a for loop

--- main.py
+++ main.py
@@ -2,44 +2,54 @@
 import turtle as t
 
 screen = t.Screen()
-sha = t.Turtle("square")
-sha.color("white")
-screen.setup(width=600, height=800)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.title("My Nokia 3310 Snake Game.")
+
+# TODO 3 Create the shape of the snake by for loop
+pos_x = 0
+
+for block in range(3):
+    snake_body = t.Turtle("square")
+    snake_body.color("white")
+    snake_body.setx(pos_x)
+    pos_x -= 20
+
+
 
 screen.listen()
 
 
 # TODO 2 Create the Snake controller
+#
+# def move_forward():
+#     sha.forward(10)
+#
+#
+# def turn_left():
+#     sha.setheading(180)
+#
+#
+# def turn_right():
+#     sha.setheading(0)
+#
+#
+# def turn_down():
+#     sha.setheading(270)
+#
+#
+# def turn_up():
+#     sha.setheading(90)
 
-def move_forward():
-    sha.forward(10)
 
+# screen.onkey(key="w", fun=turn_up)
+# screen.onkey(key="a", fun=turn_left)
+# screen.onkey(key="d", fun=turn_right)
+# screen.onkey(key="s", fun=turn_down)
 
-def turn_left():
-    sha.setheading(180)
-
-
-def turn_right():
-    sha.setheading(0)
-
-
-def turn_down():
-    sha.setheading(270)
-
-
-def turn_up():
-    sha.setheading(90)
-
-
-screen.onkey(key="w", fun=turn_up)
-screen.onkey(key="a", fun=turn_left)
-screen.onkey(key="d", fun=turn_right)
-screen.onkey(key="s", fun=turn_down)
-
-while True:
-    sha.speed(10)
-    sha.penup()
-    move_forward()
+# while True:
+#     sha.speed(10)
+#     sha.penup()
+#     move_forward()
 
 screen.exitonclick()
