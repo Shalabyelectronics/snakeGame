0d164973f2f8daba2b65b71bb7bdea86e69aebdf
shalabyelectronics <mhdshalabystar@gmail.com>
2021-10-03 07:32:12 +0300

    fix the snake movement.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 
 screen = t.Screen()
 screen.listen()
-
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Nokia 3310 Snake Game.")
@@ -19,31 +18,29 @@ for block in range(3):
     snake_body.penup()
     snake_body.color("white")
     snake_body.goto(parts_positions[block])
-    snake_body.setheading(270)
     snake_parts.append(snake_body)
-screen.update()
 
 
 # TODO 2 Create the Snake controller
 
 def move_forward():
-    part.forward(5)
+    snake_body.forward(5)
 
 
 def turn_left():
-    part.setheading(180)
+    snake_body.setheading(180)
 
 
 def turn_right():
-    part.setheading(0)
+    snake_body.setheading(0)
 
 
 def turn_down():
-    part.setheading(270)
+    snake_body.setheading(270)
 
 
 def turn_up():
-    part.setheading(90)
+    snake_body.setheading(90)
     print("here")
 
 
@@ -51,22 +48,22 @@ def turn_up():
 
 def snake_border(snake):
     if snake.heading() == 0:
-        if snake.xcor() >= 290:
+        if snake.xcor() >= 270:
             return False
         else:
             return True
     elif snake.heading() == 90:
-        if snake.ycor() == 300:
+        if snake.ycor() >= 280:
             return False
         else:
             return True
     elif snake.heading() == 180:
-        if snake.xcor() == -300:
+        if snake.xcor() >= -280:
             return False
         else:
             return True
     elif snake.heading() == 270:
-        if snake.ycor() == -290:
+        if snake.ycor() >= -270:
             return False
         else:
             return True
@@ -74,24 +71,24 @@ def snake_border(snake):
         return True
 
 
+# # TODO 4 Move snake parts forward
 
-# TODO 4 Move snake parts forward
-def snake_movement():
-    is_on = True
-    while is_on:
-        screen.update()
-        time.sleep(0.1)
-        for part in reversed(snake_parts):
-            part.forward(10)
+is_on = True
+while is_on:
+    screen.update()
+    time.sleep(0.1)
+    for part in range(len(snake_parts) - 1, 0, -1):
+        new_x = snake_parts[part - 1].xcor()
+        new_y = snake_parts[part - 1].ycor()
+        snake_parts[part].goto(new_x, new_y)
+        if not snake_border(snake_parts[part]):
+            is_on = False
+    snake_parts[0].forward(20)
+    snake_parts[0].left(90)
 
-            if not snake_border(part):
-                is_on = False
-
-
-snake_movement()
-# screen.onkey(key="w", fun=turn_up)
-# screen.onkey(key="a", fun=turn_left)
-# screen.onkey(key="d", fun=turn_right)
-# screen.onkey(key="s", fun=turn_down)
+screen.onkey(key="w", fun=turn_up)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="s", fun=turn_down)
 
 screen.exitonclick()
