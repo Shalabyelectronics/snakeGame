98d3ea877e4cb13fb8ebbe55a103dc853796015d
shalabyelectronics <mhdshalabystar@gmail.com>
2021-10-11 14:14:22 +0300

    Update my snake increase method to hide the food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # TODO 1 Create the snake body
-from snake import Snake, segments
+from snake import Snake
 from turtle import Screen
 from food import Food
 import time
@@ -14,23 +14,25 @@ screen.listen()
 test = Snake()
 screen.update()
 food = Food()
+
 screen.onkey(key="d", fun=test.turn_right)
 screen.onkey(key="w", fun=test.turn_up)
 screen.onkey(key="s", fun=test.turn_down)
 screen.onkey(key="a", fun=test.turn_left)
-print(test.snake_shape)
-# x = 0
-#
-# while x < 200:
-#     screen.update()
-#     time.sleep(0.1)
-#     test.move()
-#     # print(test.snake_head.distance(food))
-#     if test.snake_head.distance(food) <= 20:
-#         food.food_generator()
-#         segments += 1
-#         print(segments)
-#     x += 1
+
+
+x = 0
+
+while x < 200:
+    screen.update()
+    time.sleep(0.1)
+    test.move()
+    # print(test.snake_head.distance(food))
+    if test.snake_head.distance(food) <= 20:
+        screen.tracer(0)
+        test.snake_incress()
+        food.food_generator()
+    x += 1
 
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,6 +1,5 @@
 from turtle import Turtle
 
-POSITIONS = [0, -20, -40]
 SNAKE_SEGMENTS = []
 DISTANCE = 20
 UP = 90
@@ -8,30 +7,44 @@ DOWN = 270
 RIGHT = 0
 LEFT = 180
 segments = 3
+pos = 0
 
 
 class Snake:
     def __init__(self):
-        self.snake_shape = self.create_snake()
-        self.snake_head = self.snake_shape[0]
+        self.create_snake()
+        self.snake_head = SNAKE_SEGMENTS[0]
 
     def create_snake(self):
-        for seg in range(segments):
+        global pos
+        for _ in range(segments):
             segment = Turtle("square")
             segment.color("white")
             segment.penup()
-            segment.goto(POSITIONS[seg], 0)
+            segment.goto(pos, 0)
             SNAKE_SEGMENTS.append(segment)
+            pos -= 20
         return SNAKE_SEGMENTS
 
     def move(self):
-        for seg in range(len(self.snake_shape) - 1, 0, -1):
-            new_x = self.snake_shape[seg - 1].xcor()
-            new_y = self.snake_shape[seg - 1].ycor()
-            self.snake_shape[seg].goto(new_x, new_y)
-        self.snake_shape[0].forward(DISTANCE)
+        for seg in range(len(SNAKE_SEGMENTS) - 1, 0, -1):
+            SNAKE_SEGMENTS[seg].showturtle()
+            new_x = SNAKE_SEGMENTS[seg - 1].xcor()
+            new_y = SNAKE_SEGMENTS[seg - 1].ycor()
+            SNAKE_SEGMENTS[seg].goto(new_x, new_y)
+        SNAKE_SEGMENTS[0].forward(DISTANCE)
 
-    # TODO Controler W, S, D, A
+    def snake_incress(self):
+        global pos
+        pos -= 20
+        segment = Turtle("square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(pos, 0)
+        segment.hideturtle()
+        SNAKE_SEGMENTS.append(segment)
+
+
     def turn_right(self):
         if self.snake_head.heading() != LEFT:
             self.snake_head.setheading(RIGHT)
